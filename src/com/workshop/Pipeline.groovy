5829e221227cc60c44313b09e34fbbb045f4edef
Dicky Dwi Putra <97219877+dickydwiputra170100@users.noreply.github.com>
2022-01-27 10:19:58 +0700

    update

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -9,6 +9,9 @@ class Pipeline {
    String docker_user
    String app_port
    String pr_num
+   String dockerTool
+   String docker_registry
+   String timeout_hc
  
    Pipeline(
        String repository_name,
@@ -16,14 +19,19 @@ class Pipeline {
        String git_user,
        String docker_user,
        String app_port,
-       String pr_num
+       String pr_num,
+       String dockerTool,
+       String docker_registry,
+       String timeout_hc
    ){
        this.repository_name = repository_name
        this.branch_name = branch_name
        this.git_user = git_user
-       this.docker_user = docker_user
+      this.docker_user = docker_user
        this.app_port = app_port
        this.pr_num = pr_num
+       this.dockerTool = dockerTool
+       this.docker_registry = docker_registry
+       this.timeout_hc = timeout_hc
    }
- 
 }
